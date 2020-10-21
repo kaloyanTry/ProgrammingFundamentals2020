c77c47ee5c365c8a7ce955119dbb665b67136595
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-10-21 22:35:52 +0300

    Add files via upload

---
+++ Classes-DateTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleObject
+{
+    class Trainig
+    {
+        static void Main(string[] args)
+        {
+            string inputDate = Console.ReadLine();
+
+            string format = "d-M-yyyy";
+            CultureInfo provider = CultureInfo.InvariantCulture;
+
+            DateTime result;
+
+            result = DateTime.ParseExact(inputDate, format, provider);
+            Console.WriteLine(result.DayOfWeek);
+        }
+    }
+}
