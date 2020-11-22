e9e905c6101e2f6aa391c2536c5ac8f7661d6004
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-11-22 18:54:22 +0200

    Add files via upload

---
+++ MidExam/Backery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MidExam
+{
+    class Backery
+    {
+        static void Main(string[] args)
+        {
+            int biscuitsPerDay = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            int competingFactoryPerMonth = int.Parse(Console.ReadLine());
+
+            int totalPerDay = biscuitsPerDay * workers;
+            int totalBiscuits = 0;
+            int day = 0;
+
+            for (int i = 0; i < 30; i++)
+            {
+                day++;
+                
+                if (day % 3 == 0)
+                {
+                    int thirdDay = (int)Math.Floor(totalPerDay * 0.75);
+                    totalBiscuits += thirdDay;
+                }
+                else
+                {
+                    totalBiscuits += totalPerDay;
+                }       
+            }
+
+            Console.WriteLine($"You have produced {totalBiscuits} biscuits for the past month.");
+
+            int totalDiff = totalBiscuits - competingFactoryPerMonth;
+
+            double percentage = (double)Math.Abs(totalDiff) / (double)competingFactoryPerMonth * 100;
+
+            if (totalBiscuits > competingFactoryPerMonth)
+            {
+                Console.WriteLine($"You produce {percentage:F2} percent more biscuits.");
+            }
+            else
+            {
+                Console.WriteLine($"You produce {percentage:F2} percent less biscuits.");
+            }
+        }
+    }
+}
