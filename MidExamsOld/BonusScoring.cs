a20df3b4b64115bf11a636f466d9b05ad4432674
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-11-26 21:42:47 +0200

    Add files via upload

---
+++ MidExamsOld/BonusScoring.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1.BonusScoringSystem
+{
+    class BonusScoring
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+            int countOfLectures = int.Parse(Console.ReadLine());
+            int initialBonus = int.Parse(Console.ReadLine());
+
+            double maxBonus = 0;
+            int maxAttendences = 0;
+
+            for (int i = 1; i <= countOfStudents; i++)
+            {
+                int attendances = int.Parse(Console.ReadLine());
+
+                double totalBonus = (attendances / (double)countOfLectures) * (5 + initialBonus);
+
+                if (totalBonus > maxBonus)
+                {
+                    maxBonus = totalBonus;
+                    maxAttendences = attendances;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Round(maxBonus)}.");
+            Console.WriteLine($"The student has attended {maxAttendences} lectures.");
+        }
+    }
+}
---
+++ MidExamsOld/Cozonacs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1.EasterCozonacs
+{
+    class Cozonacs
+    {
+        static void Main(string[] args)
+        {
+            double boudget = double.Parse(Console.ReadLine());
+            double priceFloor = double.Parse(Console.ReadLine());
+            double pricePackEggs = priceFloor * 0.75;
+            double priceMilk = priceFloor * 1.25;
+            int countOfCozonacs = 0;
+            int coloredEggs = 0;
+
+            double priceOneCozonac = priceFloor + pricePackEggs + (priceMilk / 4);
+
+            while(boudget >= priceOneCozonac)
+            {
+                boudget -= priceOneCozonac;
+                countOfCozonacs++;
+                coloredEggs += 3;
+
+                if (countOfCozonacs % 3 == 0)
+                {
+                    int lostEggs = countOfCozonacs - 2;
+                    coloredEggs -= lostEggs;
+                }
+            }
+
+            Console.WriteLine($"You made {countOfCozonacs} cozonacs! Now you have {coloredEggs} eggs and {boudget:f2}BGN left.");
+        }
+    }
+}
