626fe25acc0dea8756a4edad597e622e498ece14
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-01 22:06:12 +0200

    Add files via upload

---
+++ FinalExamsOld/FancyBarcode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcode
+{
+    class FancyBarcode
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"@#+([A-Z][A-Za-z0-9]{4,}[A-Z]@#+)");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string barcodeInput = Console.ReadLine();
+                Match match = regex.Match(barcodeInput);
+
+                if (!match.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+
+                string barcode = match.Groups[1].Value;
+                StringBuilder group = new StringBuilder();
+
+                for (int j = 0; j < barcode.Length; j++)
+                {
+                    if (char.IsDigit(barcode[j]))
+                    {
+                        group.Append(barcode[j]);
+                    }
+                }
+
+                if (String.IsNullOrEmpty(group.ToString()))
+                {
+                    group.Append("00");
+                }
+                Console.WriteLine($"Product group: {group}");
+            }
+        }
+    }
+}
---
+++ FinalExamsOld/PasswordReseter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace PasswordReset
+{
+    class PasswordReseter
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            string command = Console.ReadLine();
+           
+            while (command != "Done")
+            {
+                if (command.Contains("TakeOdd"))
+                {
+                    StringBuilder oddChars = new StringBuilder();
+                    for (int i = 1; i < password.Length; i += 2)
+                    {
+                        oddChars.Append(password[i]);
+                    }
+
+                    password = oddChars.ToString();
+                    Console.WriteLine(password);
+                }
+                else if (command.Contains("Cut"))
+                {
+                    string[] splitted = command.Split();
+                    int index = int.Parse(splitted[1]);
+                    int length = int.Parse(splitted[2]);
+
+                    password = password.Remove(index, length);
+                    Console.WriteLine(password);
+                }
+                else if (command.Contains("Substitute"))
+                {
+                    string[] splitted = command.Split();
+                    string oldString = splitted[1];
+                    string newString = splitted[2];
+
+                    if (password.IndexOf(oldString) >= 0)
+                    {
+                        password = password.Replace(oldString, newString);
+                        Console.WriteLine(password);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
