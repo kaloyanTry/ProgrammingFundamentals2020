a7ef530495b9661028a56059df118a6f0ce7f7a0
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-03 22:22:54 +0200

    Add files via upload

---
+++ FinalExamsOld/ActivationKeys.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ActivationKeys
+{
+    class ActivationKeys
+    {
+        static void Main(string[] args)
+        {
+            string actKey = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command != "Generate")
+            {
+                string[] tokens = command.Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "Contains":
+                        if (actKey.IndexOf(tokens[1]) != -1)
+                        {
+                            Console.WriteLine($"{actKey} contains {tokens[1]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Substring not found!");
+                        }
+                        break;
+
+                    case "Flip":
+                        int startIndex = int.Parse(tokens[2]);
+                        int endIndex = int.Parse(tokens[3]);
+                        string firstPart = actKey.Substring(0, startIndex);
+                        string secondPart = actKey.Substring(startIndex, endIndex - startIndex);
+                        string thirdPart = actKey.Substring(endIndex);
+                        if (tokens[1].ToUpper() == "UPPER")
+                        {
+                            secondPart = secondPart.ToUpper();
+                        }
+                        else
+                        {
+                            secondPart = secondPart.ToLower();
+                        }
+                        actKey = firstPart + secondPart + thirdPart;
+                        Console.WriteLine(actKey);
+                        break;
+
+                    case "Slice":
+                        startIndex = int.Parse(tokens[1]);
+                        endIndex = int.Parse(tokens[2]);
+
+
+                        actKey = actKey.Remove(startIndex, endIndex - startIndex);
+                        Console.WriteLine(actKey);
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your activation key is: {actKey}");
+        }
+    }
+}
---
+++ FinalExamsOld/EmojiDetector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmojiDetector
+{
+    class EmojiDetector
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string patternNums = @"\d";
+            Regex regexNums = new Regex(patternNums);
+            long coolTreshold = 1;
+            regexNums.Matches(input).Select(t => t.Value).Select(int.Parse).ToList().ForEach(x => coolTreshold *= x);
+
+            Console.WriteLine($"Cool threshold: {coolTreshold}");
+
+            string patternEmojis = @"(:{2}|\*{2})([A-Z][a-z]{2,})\1";
+            Regex regexEmojis = new Regex(patternEmojis);
+
+            var matchesEmojis = regexEmojis.Matches(input);
+            int totalEmojis = matchesEmojis.Count;
+
+            List<string> coolEmojis = new List<string>();
+
+            foreach (Match match in matchesEmojis)
+            {
+                long coolIndex = match.Value.Substring(2, match.Value.Length - 4).ToCharArray().Sum(x => (int)x);
+
+                if (coolIndex >= coolTreshold)
+                {
+                    coolEmojis.Add(match.Value);
+                }
+            }
+
+            Console.WriteLine($"{totalEmojis} emojis found in the text. The cool ones are:");
+
+            foreach (var cool in coolEmojis)
+            {
+                Console.WriteLine(cool);
+            }
+        }
+    }
+}
