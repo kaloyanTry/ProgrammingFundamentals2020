0e2c327b1d4ff30a336baaf2136bea05feb9cc43
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-05 10:51:27 +0200

    Add files via upload

---
+++ FinalExamsOld/NeedForSpeed3.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeedForSpeed
+{
+    class NeedForSpeed3
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputCars = Console.ReadLine().Split('|');
+                string name = inputCars[0];
+                int mileage = int.Parse(inputCars[1]);
+                int fuel = int.Parse(inputCars[2]);
+
+                Car car = new Car() { Mileage = mileage, Fuel = fuel };
+                cars.Add(name, car);
+            }
+
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                string[] inputCommands = input.Split(" : ");
+                string command = inputCommands[0];
+                string car = inputCommands[1];
+
+                if (command == "Drive")
+                {
+                    int distance = int.Parse(inputCommands[2]);
+                    int fuelDrive = int.Parse(inputCommands[3]);
+
+                    if (cars[car].Fuel - fuelDrive > 0)
+                    {
+                        cars[car].Fuel -= fuelDrive;
+                        cars[car].Mileage += distance;
+                        Console.WriteLine($"{car} driven for {distance} kilometers. {fuelDrive} liters of fuel consumed.");
+
+                        if (cars[car].Mileage > 100000)
+                        {
+                            cars.Remove(car);
+                            Console.WriteLine($"Time to sell the {car}!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+                }
+                else if (command == "Refuel")
+                {
+                    int fuelRefuel = int.Parse(inputCommands[2]);
+                    if (cars[car].Fuel + fuelRefuel > 75)
+                    {
+                        fuelRefuel = 75 - cars[car].Fuel;
+                    }
+                    cars[car].Fuel += fuelRefuel;
+
+                    Console.WriteLine($"{car} refueled with {fuelRefuel} liters");
+                }
+                else if (command == "Revert")
+                {
+                    int kilometers = int.Parse(inputCommands[2]);
+                    cars[car].Mileage -= kilometers;
+                    if (cars[car].Mileage < 10000)
+                    {
+                        cars[car].Mileage = 10000;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{car} mileage decreased by {kilometers} kilometers");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            var sortedCars = cars.OrderByDescending(c => c.Value.Mileage).ThenBy(c => c.Key);
+
+            foreach (var car in sortedCars)
+            {
+                Console.WriteLine($"{car.Key} -> Mileage: {car.Value.Mileage} kms, Fuel in the tank: {car.Value.Fuel} lt.");
+            }
+        }
+    }
+
+    class Car
+    {
+        public int Mileage { get; set; }
+        public int Fuel { get; set; }
+    }
+}
---
+++ FinalExamsOld/WordTour.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace WordTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string planTravel = Console.ReadLine();
+
+            string input = Console.ReadLine();
+            while (input != "Travel")
+            {
+                string[] inputData = input.Split(':');
+                string command = inputData[0];
+
+                if (command == "Add Stop")
+                {
+                    int index = int.Parse(inputData[1]);
+                    string insertString = inputData[2];
+
+                    if (index >= 0 && index < planTravel.Length)
+                    {
+                        planTravel = planTravel.Insert(index, insertString);
+                    }
+                    Console.WriteLine(planTravel);
+                }
+                else if (command == "Remove Stop")
+                {
+                    int startIndex = int.Parse(inputData[1]);
+                    int endIndex = int.Parse(inputData[2]);
+                    bool isValid = (startIndex >= 0 && startIndex < planTravel.Length)  && (endIndex > 0 && endIndex <= planTravel.Length - 1);
+                    if (isValid)
+                    {
+                        planTravel = planTravel.Remove(startIndex, endIndex + 1 - startIndex);  
+                    }
+                    Console.WriteLine(planTravel);
+                }
+                else if (command == "Switch")
+                {
+                    string oldString = inputData[1];
+                    string newString = inputData[2];
+
+                    if (planTravel.Contains(oldString))
+                    {
+                        planTravel = planTravel.Replace(oldString, newString);
+                        
+                    }
+                    Console.WriteLine(planTravel);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {planTravel}");
+        }
+    }
+}
