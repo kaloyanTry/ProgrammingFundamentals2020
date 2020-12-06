86a53064247935c65a8e6cd6e6df9d5ec385fdaf
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-06 18:39:05 +0200

    Add files via upload

---
+++ FinalExamsOld/AdAstra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    class AdAstra
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(#|\|)(?<item>[A-Za-z\s]+)(\1)(?<date>[0-9]{2}\/[0-9]{2}\/[0-9]{2})(\1)(?<calories>[0-9]{1,5})(\1)";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+            int totalCalories = 0;
+
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                int currCalories = int.Parse(match.Groups["calories"].Value);
+                totalCalories += currCalories;
+            }
+
+            int days = totalCalories / 2000;
+            Console.WriteLine($"You have food to last you for: {days} days!");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Item: {match.Groups["item"].Value}, Best before: {match.Groups["date"].Value}, Nutrition: {match.Groups["calories"].Value}");
+            }
+        }
+    }
+}
