c74c587ecc22150af905ed6da3c665341521c606
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-10 10:54:11 +0200

    Add files via upload

---
+++ TextProcessing/EncrypetText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace CeaserCipher
+{
+    class EncrypetText
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string encrypetText = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                encrypetText += (char)(input[i] + 3);
+            }
+
+            Console.WriteLine(encrypetText.ToString());
+
+
+            //second:
+            //char[] outText = Console.ReadLine().Select(x => x + 3).Select(x => (char)x).ToArray();
+            //Console.WriteLine(string.Join("", outText));
+        }
+    }
+}
