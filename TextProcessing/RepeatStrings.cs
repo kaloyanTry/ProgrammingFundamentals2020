8ba77e81aec600122283f1ea851a8306aad27d7b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-11 21:52:52 +0200

    Add files via upload

---
+++ TextProcessing/RepeatStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatStrings
+{
+    class RepeatStrings
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split();
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                int countChars = word.Length;
+                for (int i = 0; i < countChars; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
