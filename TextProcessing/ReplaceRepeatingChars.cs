33ec2f1fee5e6b43901eb5d0ee2d8863b4351885
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-11 21:53:12 +0200

    Add files via upload

---
+++ TextProcessing/ReplaceRepeatingChars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReplaceRepeatingChars
+{
+    class ReplaceREpeatingChars
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] inputChars = input.ToCharArray();
+
+            List<char> result = new List<char>();
+            result.Add(input[0]);
+
+            foreach (var ch in inputChars)
+            {
+                if (ch != result.Last())
+                {
+                    result.Add(ch);
+                }
+            }
+
+            Console.WriteLine(string.Join(string.Empty, result));
+        }
+    }
+}
