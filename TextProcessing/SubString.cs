d0381cc2daa3917d97466db85dda4cd26f6848ac
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-12 09:01:49 +0200

    Add files via upload

---
+++ TextProcessing/SubString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SubString
+{
+    class SubString
+    {
+        static void Main(string[] args)
+        {
+            string removeString = Console.ReadLine().ToLower();
+            string text = Console.ReadLine().ToLower();
+
+            int index = text.IndexOf(removeString);
+            while (index != -1)
+            {
+                text = text.Remove(index, removeString.Length);
+                index = text.IndexOf(removeString);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
