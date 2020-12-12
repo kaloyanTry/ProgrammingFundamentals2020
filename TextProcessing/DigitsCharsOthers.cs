c130e73515f40e972f6235e3203cd88f7548a4f2
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-12 09:01:14 +0200

    Add files via upload

---
+++ TextProcessing/DigitsCharsOthers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace StringOfDigitsLettersOther
+{
+    class DigitsCharsOthers
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder chars = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    digits.Append(text[i]);
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    chars.Append(text[i]);
+                }
+                else
+                {
+                    others.Append(text[i]);
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(chars);
+            Console.WriteLine(others);
+        }
+    }
+}
---
+++ TextProcessing/StringExplosion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringExplosion
+{
+    class StringExplosion
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int power = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    power += int.Parse(input[i + 1].ToString());
+
+                }
+                else if (power > 0)
+                {
+                    input = input.Remove(i, 1);
+                    power--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
