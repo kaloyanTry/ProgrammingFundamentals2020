22ce996853f2e1412d0a435c75c17659e0169f8c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-12 09:02:38 +0200

    Add files via upload

---
+++ TextProcessing/ValidNames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidUsernames
+{
+    class ValidNames
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            List<string> validUsers = new List<string>();
+
+            foreach (var name in input)
+            {
+                if (name.Length >= 3 && name.Length <= 16)
+                {
+                    bool isValid = true;
+                    for (int i = 0; i < name.Length; i++)
+                    {
+                        if (!(char.IsLetterOrDigit(name[i]) || name.Contains('-') || name.Contains('_')))
+                        {
+                            isValid = false;  
+                        }
+                    }
+                    if (isValid)
+                    {
+                        validUsers.Add(name);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, validUsers));
+        }
+    }
+}
