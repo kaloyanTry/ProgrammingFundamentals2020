d35be00d6bc2257403b985f8ded73783e74374ed
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-12 09:02:15 +0200

    Add files via upload

---
+++ TextProcessing/TextFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TextFilter
+{
+    class TextFilter
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                if (text.Contains(word))
+                {
+                    string replace = new string('*', word.Length);
+
+                    text = text.Replace(word, replace);
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ TextProcessing/TreasureFinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TreasureFinder
+{
+    class TreasureFinder
+    {
+        static void Main(string[] args)
+        {
+            int[] keyNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray(); ;
+
+            string input = Console.ReadLine();
+
+            while (input != "find")
+            {
+                StringBuilder message = new StringBuilder();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    message.Append(Convert.ToChar(input[i] - keyNumbers[i % keyNumbers.Length]));
+                }
+
+                string messageAsString = message.ToString();
+
+                int startIndTreasure = messageAsString.IndexOf('&') + 1;
+                int lengthTreasure = messageAsString.LastIndexOf('&') - startIndTreasure;
+                string treasure = messageAsString.Substring(startIndTreasure, lengthTreasure);
+
+                int startIndCoordinates = messageAsString.IndexOf('<') + 1;
+                int lengthCoordinates = messageAsString.LastIndexOf('>') - startIndCoordinates;
+                string coordinates = messageAsString.Substring(startIndCoordinates, lengthCoordinates);
+
+                Console.WriteLine($"Found {treasure} at {coordinates}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
