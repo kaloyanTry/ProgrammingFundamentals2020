900a5b34bcea875e407243db1093268400c72815
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-16 08:45:04 +0200

    Add files via upload

---
+++ MidExamsOld/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.Numbers
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            double avrNumbers = numbers.Average();
+
+            List<int> result = numbers.OrderByDescending(x => x).Where(x => x > avrNumbers).Take(5).ToList();
+
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.Write(string.Join(" ", result));
+            }    
+        }
+    }
+}
