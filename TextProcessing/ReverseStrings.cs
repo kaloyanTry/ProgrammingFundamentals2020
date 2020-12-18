1874c0a2df06b2f26f8f5db1a180ce74d1c8d430
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-18 10:42:06 +0200

    Add files via upload

---
+++ TextProcessing/ReverseStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseStrings
+{
+    class ReverseStrings
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            while (text != "end")
+            {
+                string reversedText = "";
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    reversedText += text[i];
+                }
+
+                Console.WriteLine($"{text} = {reversedText}");
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
