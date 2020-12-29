ff38bf720cb8754f95845e30ee9ce3be296d708e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-29 08:23:16 +0200

    Add files via upload

---
+++ TextProcessing/StringsMultiplier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CharMultiplier
+{
+    class StringsMultiplier
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(CharMultiplier(input[0], input[1]));
+        }
+
+        static int CharMultiplier(string firstString, string secondString)
+        {
+            int sum = 0;
+
+            string longestString = string.Empty;
+            string shortestString = string.Empty;
+
+            if (firstString.Length > secondString.Length)
+            {
+                longestString = firstString;
+                shortestString = secondString;
+            }
+            else
+            {
+                longestString = secondString;
+                shortestString = firstString;
+            }
+
+            for (int i = 0; i < shortestString.Length; i++)
+            {
+                sum += shortestString[i] * longestString[i];
+            }
+
+            for (int i = shortestString.Length; i < longestString.Length; i++)
+            {
+                sum += longestString[i];
+            }
+
+            return sum;
+        }
+    }
+}
