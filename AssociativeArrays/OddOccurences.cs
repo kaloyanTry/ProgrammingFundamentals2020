a57ae6e1de0ab36b8a0959abb6469de5c2f72875
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-09 08:47:46 +0200

    Odd Occurrences
    Odd Occurrences of words in a string array of words

---
+++ AssociativeArrays/OddOccurences.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOccurences
+{
+    class OddOccurences
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 == 1)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
