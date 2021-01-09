02a3f42e9c062b1b6a82adc60418bcf1d7f20411
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-09 08:25:32 +0200

    MinerTask

---
+++ AssociativeArrays/MinerTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinerTask
+{
+    class MinerTask
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resoursesList = new Dictionary<string, int>();
+          
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+                
+                if (resoursesList.ContainsKey(resource))
+                {
+                    resoursesList[resource] += quantity;
+                }
+                else
+                {
+                    resoursesList.Add(resource, quantity);
+                }
+            }
+
+            foreach (var pair in resoursesList)
+            {
+                Console.WriteLine(pair.Key + " -> " + pair.Value);
+            }
+        }
+    }
+}
